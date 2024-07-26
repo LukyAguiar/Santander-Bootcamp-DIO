0c2f36cffed5857e56fda03ff08e524073cc72d3
lucas.nune@outlook.com.br <Lucas>
2024-07-26 13:51:43 -0300

    Setter e Getter

--- POO/visibilidadeDosRecursos/src/Estabelecimento.java
+++ POO/visibilidadeDosRecursos/src/Estabelecimento.java
@@ -5,50 +5,20 @@ import com.lanchonete.atendimento.cozinha.Cozinheiro;
 
 public class Estabelecimento {
     public static void main(String[] args) {
-        Cozinheiro   cozinheiro = new Cozinheiro();
-        //ações que não precisam estarem disponíveis para toda a aplicação
-        cozinheiro.lavarIngredientes();
-        cozinheiro.baterVitaminaLiquidificador();
-        cozinheiro.selecionarIngredientesVitamina();
-        cozinheiro.prepararLanche();
-        cozinheiro.prepararVitamina();
-        cozinheiro.prepararVitamina();
-
-        //ações que estabelecimento precisa ter ciência
-        cozinheiro.adicionarSucoNoBalcao();;
-        cozinheiro.adicionarLancheNoBalcao();
-        cozinheiro.adicionarComboNoBalcao();
-
-        Almoxarife almoxarife = new Almoxarife();
-        //ações que não precisam estarem disponíveis para toda a aplicação
-        almoxarife.controlarEntrada();
-        almoxarife.controlarSaida();
-        //ação que somente o seu pacote cozinha precisa conhecer (default)
-        almoxarife.entregarIngredientes();
-        almoxarife.trocarGas();
-
-        Atendente atendente = new Atendente();
-        atendente.pegarLancheCozinha();
-        atendente.receberPagamento();
-        atendente.servindoMesa();
-        //ação que somente o seu pacote cozinha precisa conhecer (default)
-        atendente.trocarGas();
 
         Cliente cliente = new Cliente();
         cliente.escolherLanche();
         cliente.fazerPedido();
         cliente.pagarConta();
 
-        //não deveria, mas o estabelecimento
-        //ainda não definiu normas de atendimento
-        cliente.pegarPedidoBalcao();
+        Atendente atendente = new Atendente();
+        atendente.receberPagamento();
 
-        //esta ação é muito sigilosa, qua tal ser privada ?
-        cliente.consultarSaldoAplicativo();
+        Cozinheiro cozinheiro = new Cozinheiro();
+        cozinheiro.adicionarSucoNoBalcao();;
+        cozinheiro.adicionarLancheNoBalcao();
+        cozinheiro.adicionarComboNoBalcao();
 
-        //já pensou os clientes ouvindo que o gás acabou ?
-        cozinheiro.pedirParaTrocarGas(atendente);
-        cozinheiro.pedirParaTrocarGas(almoxarife);
-
+        atendente.servindoMesa();
     }
 }
